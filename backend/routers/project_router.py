58c9c1330719d2b9cec4a3eec1c6864e75d8a02a
jbbdyee <ginggapple@gmail.com>
2026-07-28 17:24:58 +0900

    new

--- backend/routers/project_router.py
+++ backend/routers/project_router.py
@@ -3,29 +3,29 @@ from fastapi import APIRouter
 from backend.schemas.project_schema import *
 from backend.services.project_service import *
 
-projects_router = APIRouter()
+router = APIRouter()
 
 #  1. create
-@projects_router.post("/projects/create")
+@router.post("/projects")
 def study_create(projects: ProjectCreate) ->ProjectGet:
     return study_create(projects)
 
 #  2. 한개 조회
-@projects_router.get("/projects/get/{projects_id}")
+@router.get("/projects/{projects_id}")
 def get(projects_id: int) -> ProjectGet:
     return projects_get(projects_id)
 
 #  3. 전체 조회
-@projects_router.get("/study/getall")
+@router.get("/projects/getall")
 def get_all() -> list[ProjectGet]:
     return projects_get_all()
 
 #  4. 한개 삭제
-@projects_router.delete("/projects/delete/{projects_id}")
+@router.delete("/projects/delete/{projects_id}")
 def delete(projects_id: int) -> dict:
     return projects_delete(projects_id)
 
 #  5. 수정
-@projects_router.put("/projects/update")
+@router.put("/projects/update")
 def put(projects: ProjectUpdate) -> ProjectGet:
     return projects_update(projects)
